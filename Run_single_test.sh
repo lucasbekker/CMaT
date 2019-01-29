2e9eb2e7c9a30020b26a9545a642caea86b1bd79
Lucas Bekker <lucasbekker@fastmail.nl>
2019-01-29 15:12:21 +0100

    Added test script that runs a single test.

---
+++ Run_single_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script builds and runs the test provided by the argument.
+
+# Activate the environment variables of MKL.
+source /opt/intel/compilers_and_libraries/linux/mkl/bin/mklvars.sh intel64
+
+# Compiler flags.
+MKL_INCLUDE="-I${MKLROOT}/include"
+MKL_COMPILER="-m64"
+MKL_LINKER="-L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_tbb_thread -lmkl_core -ltbb -lstdc++ -lpthread -lm -ldl"
+CUDA_LINKER="-lcublas"
+GPP_COMPILER="-O3"
+
+called_test=$1
+
+# Build.
+echo "Building" $called_test
+nvcc tests/$called_test -o tests/bin/$called_test $GPP_COMPILER $MKL_INCLUDE $MKL_COMPILER $MKL_LINKER $CUDA_LINKER
+
+# Run.
+echo "Running" $called_test
+tests/bin/$called_test
